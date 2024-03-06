4bd24faf6d1dcea292315e453e5ca7c6b26ff466
Kid <hi@xuann.wang>
2024-03-07 01:03:00 +0800

    Add GitHub File Tree

--- api/index.ts
+++ api/index.ts
@@ -2,9 +2,11 @@ import {Router} from 'itty-router'
 import {createServerAdapter} from '@whatwg-node/server'
 
 import {githubFileContributors} from '../routes/api/github-file-contributors.js'
+import {githubFileTree} from '../routes/github-file-tree.js'
 
 const router = Router()
 
 router.get('/api/github-file-contributors', githubFileContributors)
+router.get('/github-file-tree', githubFileTree)
 
 export default createServerAdapter(router.handle)
